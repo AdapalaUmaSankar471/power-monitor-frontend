3dc251c11c2eb41664a531f4fabf58589f0ded19
AdapalaUmaSankar471 <venkatanagavenuavula@gmail.com>
2026-03-19 07:35:46 +0530

    Force redeploy

--- src/pages/ViewerDashboard.js
+++ src/pages/ViewerDashboard.js
@@ -62,7 +62,8 @@ function ViewerDashboard() {
   // WebSocket
   // ============================
   useEffect(() => {
-    const socket = new SockJS("http://localhost:8080/power-monitor");
+    const wsUrl = (process.env.REACT_APP_API_URL || 'http://localhost:8080').replace('http://', 'https://');
+    const socket = new SockJS(wsUrl + '/power-monitor');
 
     const client = new Client({
       webSocketFactory: () => socket,
